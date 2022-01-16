01ca7c4f4278865b27db6e4b04bc38e364b710ff
Matheus Lopes <matlopes1999@gmail.com>
2022-01-16 12:26:39 -0300

    Create the cart module scaffold, update dependencies of products

---
+++ cart/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
+module.exports = {
+    mode: 'development',
+    devServer: {
+        port: 8082
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'cart',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './CartShow': path.join(__dirname, 'src', 'index')
+            }
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'public', 'index.html')
+        })
+    ]
+};
